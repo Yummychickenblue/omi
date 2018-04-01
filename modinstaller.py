49c3315b78d2e4c01d57927fdd01b4f6f6ecebf9
Jacob Ham <35405092+Yummychickenblue@users.noreply.github.com>
2018-03-31 21:50:17 -0400

    added comments and removed extranous print functions
    Added comments to nearly all functions, changed the positioning so that more generic functions are near the top, as well as removed some unnecessary print functions from test_relevant().

--- modinstaller.py
+++ modinstaller.py
@@ -1,16 +1,33 @@
 import os
 import shutil
 
+#Writes an entry to openmw.cfg.
+def write_openmwcfg(arg):
+    with open(openmw_home() + '/openmw.cfg', 'a') as f:
+        f.write('data="' + openmw_home() 
+                + '/mods/'
+                + get_folder_name(arg)
+                +'"')
+
+#Returns the name of the folder selected by the user.
+#Used to name the directory that mod files get copied to.
+#Example if someone gives the folder "Empire Alpha" this will return
+#"Empire Alpha".
+def get_folder_name(arg):
+    dir_list = arg.split('/')
+    name = dir_list[-1]
+    return(name)
+
+#Returns the directory of OpenMW's installation,
+#assuming it's installed in the default location.
 def openmw_home():
     return(os.environ.get('HOME') + '/.config/openmw')
 
-
-
-#This function breaks down if someone puts any of the entries in 
-#relevant_files in their mods title. I try to mititgate this by 
-#testing for Data Files first. Of course some people don't wrap their 
-#mods in data files dirs and that makes my life hard. In future I'm 
-#thinking of this to verify length for the folder names
+#Tests to see if there are any mod files in the given directory of arg.
+#Theoretically it breaks down if someone doesn't puts these words in
+#in their mod name and doesn't wrap their files in a Data Files
+#directory. In the future I'm thinking of verifying the length of
+#folder names to protect against hat.
 def test_relevant(arg):
     relevant_files = ('.esm',
                      '.esp',
@@ -30,16 +47,18 @@ def test_relevant(arg):
         flist.append(f)
         fset.update(set(flist))
     if len(fset) > 1:
-        print(flist)
-        print(fset)
-        print('test_relevant returned true')
         return True
     else:
-        print(fset)
-        print('test_relevant returned false')
         return False
     
-
+#This function is used to put the system in the directory where the
+#mod files are located. First it tries to change to a Data Files dir
+#to avoid running the above function. If that fails it moves down the
+#first directory in os.listdir() until it can't go farther, at which 
+#point it goes back to the top and goes down the next tree ad infinitum.
+#As of now the only way to get insight into this function's status is to
+#watch the log. Eventually gui cues should be added to reflect what 
+#this is doing.
 def normalize(arg):
     i = 0
     dir = os.listdir()[i]
@@ -78,20 +97,7 @@ def install(dir):
     except FileExistsError:
         print('The mod already exists in') 
     
-def get_folder_name(arg):
-    dir_list = arg.split('/')
-    name = dir_list[-1]
-    return(name)
 
-#Come back here and later and add support for writing to other places
-#than the end of the file.
-def write_openmwcfg(arg):
-    with open(openmw_home() + '/openmw.cfg', 'a') as f:
-        f.write('data="' + openmw_home() 
-                + '/mods/'
-                + get_folder_name(arg)
-                +'"')
     
 if __name__ == '__main__':
     pass
-            
